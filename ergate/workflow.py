b544110683afb4a673b7d15ed57d008c5f9380cd
Steven Gilroy <steven.gilroy@packetfabric.com>
2024-12-02 09:50:31 -0600

    `workflow.py`: Update debug output.

--- ergate/workflow.py
+++ ergate/workflow.py
@@ -52,54 +52,83 @@ class Workflow:
         initial: bool = False,
         path: WorkflowPath | None = None,
     ) -> list[list[WorkflowPathTypeHint]]:
+        print("===111.01===", [index, depth])
+
         if not initial:
+            print("===111.02===", [index, depth])
+
             if path is None:
+                print("===111.03==", [index, depth])
+
                 err = (
                     f"Failed to calculate workflow path from step {index}: "
                     f"path cannot be null."
                 )
                 raise ValueError(err)
 
+            print("===111.04==", [index, depth])
+
             if path not in self[index].paths:
+                print("===111.05==", [index, depth])
+
                 err = (
                     f"Failed to calculate workflow path from step {index}: "
                     f"path not registered: {path}"
                 )
                 raise ValueError(err)
 
+            print("===111.06==", [index, depth])
+
             next_index = self._find_next_step(index, path)
         else:
+            print("===111.07==", [index, depth])
+
             path = NextStepPath()
             next_index = index
+
+        print("===111.08==", [index, depth], next_index)
 
         current_step = (path, index)
         paths: list[list[WorkflowPathTypeHint]] = []
 
+        print("===111.09==", [index, depth], current_step)
+
         if depth >= max(len(self) * 3, 30):
+            print("===111.10==", [index, depth], max(len(self) * 3, 30))
             LOG.warning(
                 "Aborting current path calculation due to potential infinite loop: "
                 f"(depth: {depth})"
             )
-            print("===411.1===", index, depth, len(paths), [[p[1] for p in path] for path in paths])
+            print("===411.1===", [index, depth], len(paths), [[p[1] for p in path] for path in paths])
             return paths
 
         if next_index >= len(self):
+            print("===111.11===", [index, depth])
+
             paths.append([current_step])
-            print("===411.2===", index, depth, len(paths), [[p[1] for p in path] for path in paths])
+            print("===411.2===", [index, depth], len(paths), [[p[1] for p in path] for path in paths])
+            print("===111.12===", [index, depth], paths)
             return paths
 
-        for next_path in self[next_index].paths:
-            print("===421.1===", index, depth, "next_path:", next_path)
-            # TODO: Should there be an `if not already in self._paths` to avoid duplicate calcuations?
+        for ii, next_path in enumerate(self[next_index].paths):
+            print("===111.13===", [index, depth], ii, next_path)
+            print("===421.1===", [index, depth], "next_path:", next_path)
+
+            # TODO: Should there be an `if not already in self._paths` to avoid duplicate calculations?
             paths += self._calculate_paths(next_index, path=next_path, depth=depth + 1)
+            print("===111.14===", [index, depth], len(paths))
 
         if not initial:
             paths = [[current_step, *next_path] for next_path in paths]
+            print("===111.15===", [index, depth], len(paths))
 
-        print("===411.3===", index, depth, len(paths), [[p[1] for p in path] for path in paths])
+        print("===111.16===", [index, depth], len(paths))
+        print("===411.3===", [index, depth], len(paths), [[p[1] for p in path] for path in paths])
         return paths
 
     def calculate_paths(self, index: int) -> list[list[WorkflowPathTypeHint]]:
+
+        print("===110.1===", index)
         return self._calculate_paths(index, initial=True)
 
     def update_paths(self) -> None:
