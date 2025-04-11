0a23547d2faf9239a4d06d30c61787a8ba8e7618
Steven Gilroy <steven.gilroy@packetfabric.com>
2025-04-11 13:24:54 -0500

    `ergate/job_status.py`: `JobStatus`: Manually define enum values in light of non-sequential definitions from `netbox-workflow`.

--- ergate/job_status.py
+++ ergate/job_status.py
@@ -2,12 +2,12 @@ from enum import IntEnum, auto
 
 
 class JobStatus(IntEnum):
-    SCHEDULED = auto()
-    PENDING = auto()
-    QUEUED = auto()
-    RUNNING = auto()
-    COMPLETED = auto()
-    FAILED = auto()
-    ABORTED = auto()
-    CANCELLING = auto()
-    CANCELLED = auto()
+    SCHEDULED = 1
+    PENDING = 7
+    QUEUED = 2
+    RUNNING = 3
+    COMPLETED = 4
+    FAILED = 5
+    ABORTED = 6
+    CANCELLING = 8
+    CANCELLED = 9
