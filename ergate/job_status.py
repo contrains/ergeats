e83a12e522bf9015dc7e92df0e9dca49f87f2a84
Steven Gilroy <steven.gilroy@packetfabric.com>
2025-04-14 16:04:58 -0500

    `ergate/job_status.py`: `JobStatus`: Change ordering of choices (move `PENDING` down).

--- ergate/job_status.py
+++ ergate/job_status.py
@@ -3,11 +3,11 @@ from enum import IntEnum, auto
 
 class JobStatus(IntEnum):
     SCHEDULED = auto()
-    PENDING = auto()
     QUEUED = auto()
     RUNNING = auto()
     COMPLETED = auto()
     FAILED = auto()
     ABORTED = auto()
+    PENDING = auto()
     CANCELLING = auto()
     CANCELLED = auto()
