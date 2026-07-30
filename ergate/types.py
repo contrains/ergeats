ca3fd2181689fc389af097e38f93540c348b1ad8
Sir Steven Gilroy <steven@theclacomputers.com>
2026-07-30 09:46:09 -0500

    Add `JSONSerializable` type hint.

--- ergate/types.py
+++ ergate/types.py
@@ -1,6 +1,6 @@
 from collections.abc import Callable
 from contextlib import AbstractContextManager
-from typing import Any, TypeVar
+from typing import Any, TypeAlias, TypeVar
 
 from .annotations import Context, Depends, Input, JobObject
 
@@ -12,3 +12,13 @@ Lifespan = Callable[[AppType], AbstractContextManager[None]] | None
 SignalHandler = Callable[[JobType], Any]
 
 Annotation = Input | Depends | Context | JobObject
+
+JSONSerializable: TypeAlias = (
+    dict[str, "JSONSerializable"]
+    | list["JSONSerializable"]
+    | str
+    | int
+    | float
+    | bool
+    | None
+)
